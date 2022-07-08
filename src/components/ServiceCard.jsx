2caec0e392a9812a693156f9eea328658095b4e1
Alexander Stenberg <alstenberg@live.se>
2022-07-08 17:24:49 +0200

    worked on service cards

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -6,8 +6,8 @@ export default function ServiceCard(props) {
       <div className="service-card__content">
         <img className="service-card__img" src={props.img} alt={props.alt} />
         <div className="service-card__text">
-          <h2>{props.service}</h2>
-          <p>{props.desc}</p>
+          <h2 className="service-card__header">{props.service}</h2>
+          <p className="service-card__desc">{props.desc}</p>
         </div>
       </div>
     </section>
